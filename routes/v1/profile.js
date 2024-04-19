6497889fbe44e89ef9ac5f905462be64a609943a
Orenico10 <oren.lev987@gmail.com>
2024-04-19 19:55:26 +0300

    update shnw version + added alternative username to uuid

--- routes/v1/profile.js
+++ routes/v1/profile.js
@@ -7,11 +7,17 @@ module.exports = wrap(async function (req, res) {
     const profileid = req.params.profileid;
     let uuid = req.params.uuid;
     if (!isUuid(uuid)) {
-        const mojang_response = await makeRequest(res, `https://api.ashcon.app/mojang/v2/user/${uuid}`);
-        if (mojang_response?.data?.uuid) {
-            uuid = mojang_response.data.uuid.replace(/-/g, '');
-        }
-    }
+		const mojang_response = await makeRequest(res, `https://api.ashcon.app/mojang/v2/user/${uuid}`).catch(
+			async (err) => {
+				return await makeRequest(res, `https://api.mojang.com/users/profiles/minecraft/${uuid}`);
+			}
+		);
+		if (mojang_response?.data?.uuid) {
+			uuid = mojang_response.data.uuid.replace(/-/g, '');
+		} else if (mojang_response?.data?.id) {
+			uuid = mojang_response.data.id.replace(/-/g, '');
+		}
+	}
 
     const playerRes = await makeRequest(res, `https://api.hypixel.net/v2/player?key=${req.query.key?.toString()}&uuid=${uuid}`);
     const player = parseHypixel(playerRes, uuid, res);
--- routes/v1/profiles.js
+++ routes/v1/profiles.js
@@ -4,19 +4,31 @@ const { makeRequest, wrap } = require('../../utils/request');
 const { parseHypixel, parseProfiles } = require('../../utils/hypixel');
 
 module.exports = wrap(async function (req, res) {
-    let uuid = req.params.uuid;
-    if (!isUuid(uuid)) {
-        const mojang_response = await makeRequest(res, `https://api.ashcon.app/mojang/v2/user/${uuid}`);
-        if (mojang_response?.data?.uuid) {
-            uuid = mojang_response.data.uuid.replace(/-/g, '');
-        }
-    }
+	let uuid = req.params.uuid;
+	if (!isUuid(uuid)) {
+		const mojang_response = await makeRequest(res, `https://api.ashcon.app/mojang/v2/user/${uuid}`).catch(
+			async (err) => {
+				return await makeRequest(res, `https://api.mojang.com/users/profiles/minecraft/${uuid}`);
+			}
+		);
+		if (mojang_response?.data?.uuid) {
+			uuid = mojang_response.data.uuid.replace(/-/g, '');
+		} else if (mojang_response?.data?.id) {
+			uuid = mojang_response.data.id.replace(/-/g, '');
+		}
+	}
 
-    const playerRes = await makeRequest(res, `https://api.hypixel.net/v2/player?key=${req.query.key?.toString()}&uuid=${uuid}`);
-    const player = parseHypixel(playerRes, uuid, res);
+	const playerRes = await makeRequest(
+		res,
+		`https://api.hypixel.net/v2/player?key=${req.query.key?.toString()}&uuid=${uuid}`
+	);
+	const player = parseHypixel(playerRes, uuid, res);
 
-    const profileRes = await makeRequest(res, `https://api.hypixel.net/v2/skyblock/profiles?key=${req.query.key?.toString()}&uuid=${uuid}`);
-    const profile = await parseProfiles(player, profileRes, uuid, res, req.query.key?.toString());
+	const profileRes = await makeRequest(
+		res,
+		`https://api.hypixel.net/v2/skyblock/profiles?key=${req.query.key?.toString()}&uuid=${uuid}`
+	);
+	const profile = await parseProfiles(player, profileRes, uuid, res, req.query.key?.toString());
 
-    return res.status(200).json({ status: 200, data: profile });
+	return res.status(200).json({ status: 200, data: profile });
 });
